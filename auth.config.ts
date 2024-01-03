90a2336aa5e373cb1dce4afff9f59a19f39dee58
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 21:26:31 +0530

    feat: added social logins

--- auth.config.ts
+++ auth.config.ts
@@ -4,9 +4,19 @@ import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "./schema";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs"
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
 providers: [
+    Github({
+    clientId:process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+    Google({
+    clientId:process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
         async authorize(credentials){
             const validatedFields = LoginSchema.safeParse(credentials)
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,10 +3,16 @@
 import { Button } from "../ui/button";
 import {FcGoogle} from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 
 export const Social = () =>{
-
+    const onClick = (provider: "google" | "github") =>{
+        signIn(provider,{
+            callbackUrl:DEFAULT_LOGIN_REDIRECT
+        })
+    }
 
     return(
     <div className="flex items-center w-full gap-x-2">
@@ -14,7 +20,7 @@ export const Social = () =>{
     size="lg"
     className="w-full cursor-pointer hover:bg-slate-900 z-10"
     variant="outline"
-    onClick={() => {}}
+    onClick={() => onClick("google")}
     >
     <FcGoogle className="h-5 w-5"/>
     </Button>
@@ -22,7 +28,7 @@ export const Social = () =>{
     size="lg"
     className="w-full hover:bg-slate-900 z-10"
     variant="outline"
-    onClick={()=>{}}
+    onClick={()=>onClick("github")}
     >
     <FaGithub className="h-5 w-5" />
     </Button>
