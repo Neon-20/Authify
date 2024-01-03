fe6fd6fff6d7274f17237f4a5b41721bca62c0f2
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 16:48:28 +0530

    feat:adding role based access

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
+import { getUserById } from './data/user';
 
 const prisma = new PrismaClient()
 
@@ -16,11 +17,22 @@ callbacks:{
     async session({token,session}){
         if(token.sub && session.user){
             session.user.id = token.sub
+        }   
+        if(token.role  && session.user){
+            session.user.role = token.role
         }
         return session
     },
 
     async jwt({token}){
+        if(!token.sub){
+            return token
+        }
+        const existingUser = await getUserById(token.sub);
+        if(!existingUser){
+            return token
+        }
+        token.role = existingUser.role
         return token
     }
 
