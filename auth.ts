53c511d98881a26af04ea73c640b8df8f1025b8a
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 20:59:31 +0530

    refactor: ensuring type safety

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, UserRole } from "@prisma/client"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
 import { getUserById } from './data/user';
@@ -14,12 +14,21 @@ export const {
     signOut
 } = NextAuth({
 callbacks:{
+    //Just trying to block myself from signing in xD
+    // async signIn({user}){
+    //     const existingUser = await getUserById(user.id);
+    //     if(!existingUser || !existingUser.emailVerified){
+    //         return false
+    //     }
+    //     return true;
+    // }
+    
     async session({token,session}){
         if(token.sub && session.user){
             session.user.id = token.sub
         }   
-        if(token.role  && session.user){
-            session.user.role = token.role
+        if(token.role && session.user){
+            session.user.role = token.role as UserRole
         }
         return session
     },
---
+++ next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth,{DefaultSession} from "next-auth";
+
+
+export type ExtendedUser = DefaultSession["user"] & {
+    role:UserRole
+}
+
+declare module "next-auth" {
+    interface Session{
+        user:ExtendedUser
+    }
+}
