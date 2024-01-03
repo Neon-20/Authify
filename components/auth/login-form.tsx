1055141a3bc2035430dfc9521feac0ddf0e70797
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 19:37:15 +0530

    refactor:types

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -39,7 +39,7 @@ export const LoginForm = () => {
         setSuccess("");
         startTransition(()=>{
         Login(values).
-        then((data) => {
+        then((data:any) => {
         setError(data.error);
         setSuccess(data.success)
         })
@@ -111,10 +111,17 @@ export const LoginForm = () => {
             size="lg"
             className="w-full hover:bg-slate-900 backdrop-blur-sm cursor-pointer"
             variant="outline" 
-            type="submit"  
-            disabled = {isPending}
+            type="submit" 
             >
-            Login
+            {isPending && (
+                <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"/>
+                </svg>
+            )}
+            {!isPending && (
+                <p>Login</p>
+            )}
             </Button>
         </form>
         </Form>
