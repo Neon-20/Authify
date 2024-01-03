2490ed48e5385e6465ab95167b1e744d59a4485f
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 15:21:12 +0530

    feat: generic routes define

---
+++ routes.ts
@@ -0,0 +1,13 @@
+export const publicRoutes = [
+    "/"
+]
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register"
+];
+
+
+export const apiAuthPrefix = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings" 
