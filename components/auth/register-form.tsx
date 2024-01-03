dbd0e91545669fec69330cd7b0a25b9149e09308
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-03 19:37:03 +0530

    style: loader icon to Login and Register

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -137,7 +137,15 @@ export const RegisterForm = () => {
             type="submit"  
             disabled = {isPending}
             >
-            Register
+            {isPending && (
+                <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"/>
+                </svg>
+            )}
+            {!isPending && (
+                <p>Register</p>
+            )}
             </Button>
         </form>
         </Form>
