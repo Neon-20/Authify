62c387cf63de6f90178d1e857f5acecc034740c4
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-08 15:34:50 +0530

    feat: added 2FA functionality in login action

--- actions/login.ts
+++ actions/login.ts
@@ -5,9 +5,12 @@ import * as z from "zod";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import {AuthError} from "next-auth";
-import { generateVerificationToken } from "@/lib/token";
+import { generateVerificationToken,generateTwoFactorToken } from "@/lib/token";
 import { getUserByEmail } from "@/data/user";
-import { sendVerificationEmail } from "@/lib/mail";
+import { sendVerificationEmail,sendTwoFactorEmail } from "@/lib/mail";
+import { getTwoFactorTokenfromEmail } from "@/data/two-factor-token";
+import { db } from "@/lib/db";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 export const Login = async(values:z.infer<typeof LoginSchema>) =>{
     //validate these fields on server side as well
@@ -21,7 +24,7 @@ export const Login = async(values:z.infer<typeof LoginSchema>) =>{
     }
 
     //If the details are validated then we have as
-    const {email,password} = validatedFields.data
+    const {email,password,code} = validatedFields.data
     const existingUser = await getUserByEmail(email);
     if(!existingUser?.email || !existingUser || !existingUser.password){ //
         return {
@@ -36,11 +39,72 @@ export const Login = async(values:z.infer<typeof LoginSchema>) =>{
         verificationToken.email,
         verificationToken.token
     )
-        
         return {    
             success:"Confirmation email sent!"
         }
     }
+
+    if(existingUser.isTwoFactorEnabled && existingUser.email){
+        if(code){
+        const twoFactorToken = await getTwoFactorTokenfromEmail(
+            existingUser.email
+        );
+        if(!twoFactorToken){
+            return{
+                error:"Invalid Code"
+            }
+        }
+        
+        if(twoFactorToken.token!==code){
+            return{
+                error:"Invalid Code"
+            }
+        }
+
+        const hasExpired = new Date(twoFactorToken.expires) < new Date();
+        if(hasExpired){
+            return{
+                error:"Code has expired"
+            }
+        }
+
+        //finally remove the 2FA code and create the new 2FA token
+        await db.twoFactorToken.delete({
+            where:{
+                id:twoFactorToken.id
+            }
+        })
+
+        const existingConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+        if(existingConfirmation){
+            await db.twoFactorConfirmation.delete({
+                where:{
+                    id:existingConfirmation.id
+                }
+            })
+        }
+        //finally if we dont have any case we will create
+        await db.twoFactorConfirmation.create({
+            data:{
+                userId:existingUser.id
+            }
+        })
+        }
+        else{
+        const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+        // console.log({
+        //     twoFactorToken
+        // })
+        //Ok so 2FA code is coming, now we have to put it inside the box from frontend side
+
+        await sendTwoFactorEmail(
+            twoFactorToken.email,
+            twoFactorToken.token
+        )
+        return {twoFactor: true}
+        }
+    }
+    
 
     try{
     await signIn("credentials",{
