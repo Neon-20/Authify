6c96db389abaa8fd680111e4cc10d803e8e11bb6
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-08 15:37:19 +0530

    refactor:modified resend-mail function for 2FA email

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,16 @@ const resend = new Resend(
     process.env.RESEND_API_KEY
 )
 
+export const sendTwoFactorEmail = async(email:string,token:string) => {
+    await resend.emails.send({
+        from:"onboarding@resend.dev",
+        to:email,
+        subject:"2FA Code",
+        html:`<p>Your 2FA Code: ${token}</p>`
+    })
+}
+
+
 export const sendPasswordResetEmail = async(email:string,token:string) => {
     const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
     await resend.emails.send({
