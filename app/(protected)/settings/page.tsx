5238afa6a1a5efb0d0cb6c8029fefb944f193218
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-09 20:39:19 +0530

    chore: added settings page things

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,26 @@
-import { auth, signOut } from "@/auth";
+// import { auth, signOut } from "@/auth";
+"use client"
+import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { signOut } from "next-auth/react";
 
-const SettingsPage = async() => {
-    const session = await auth();
-    return ( 
-        <div>
-            {JSON.stringify(session)}
-            <form action = {async() =>{
-            "use server"
-            await signOut();
-            }}>
-                <button
-                type="submit"
-                >
-                Sign Out
-                </button>
-            </form>
+const SettingsPage = () =>{
+    const session = useCurrentUser();
+    const onClick = () =>{
+        signOut();
+    }
+    return(
+        <div className="bg-white backdrop-blur-sm rounded-xl">
+        <Button
+        onClick={onClick}
+        type="submit"
+        className="items-center flex justify-center 
+        px-4 py-2 text-sm font-medium text-white hover:text-black bg-red-600"
+        >
+        SignOut
+        </Button>
         </div>
-    );
+    ) 
 }
 
-export default SettingsPage;
+export default SettingsPage
