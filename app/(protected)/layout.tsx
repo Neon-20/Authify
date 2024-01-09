64a556c1679ebd0fe4d924cd2e098a15c5747c69
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-09 20:37:47 +0530

    style:layout file for protected page

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,20 @@
+import { SparklesPreview2 } from "@/components/sparkles2";
+import NavBar from "./_components/navbar";
+
+const ProtectedLayout = ({
+    children
+}:{
+    children:React.ReactNode
+}) => {
+    return (
+        <div className="h-full w-full flex flex-col
+        items-center justify-center gap-y-10">
+    <SparklesPreview2>
+        <NavBar/>
+        {children}
+    </SparklesPreview2>
+    </div>
+    );
+}
+
+export default ProtectedLayout;
