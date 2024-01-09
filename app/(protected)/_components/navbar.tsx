d0f6a27a66d649edf7225b1d1728b5464e20a921
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-09 20:39:34 +0530

    feat: added navBar component

---
+++ app/(protected)/_components/navbar.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const NavBar = () => {
+    const pathname = usePathname();
+    return ( 
+        <nav className="
+        bg-black border shadow-md  border-purple-400 flex justify-between items-center p-4
+        rounded-xl w-[600px] backdrop-blur-sm">
+        <div className="flex gap-x-2">
+        {/* Now with this gap render all the navbar list items */}
+        <Button
+        className="hover:bg-accent"
+        variant={pathname === "/server" ? "outline" : "ghost"}
+        asChild>
+            <Link href="/server">
+            Server
+            </Link>
+        </Button>
+        <Button
+        className="hover:bg-accent"
+        variant={pathname === "/client" ? "outline" : "ghost"}
+        asChild>
+            <Link href="/client">
+            Client
+            </Link>
+        </Button>
+        <Button
+        className="hover:bg-accent"
+        variant={pathname === "/admin" ? "outline" : "ghost"}
+        asChild>
+            <Link href="/admin">
+            Admin
+            </Link>
+        </Button>
+        <Button
+        className="hover:bg-accent"
+        variant={pathname === "/settings" ? "outline" : "ghost"}
+        asChild>
+            <Link href="/settings">
+            Settings
+            </Link>
+        </Button>
+        </div>
+        <p>User Button</p>
+        </nav>
+    );
+}
+
+export default NavBar;
