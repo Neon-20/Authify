f309e4a6d53ab11b237e0091187140a97dd132ed
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-11 01:22:39 +0530

    feat: admin page front and backend

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { admin } from "@/actions/admin";
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useCurrentRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
+
+const AdminPage = () => {
+    const onApiRouteClick = async() => {
+        const response = await fetch("/api/admin")
+        if(response.ok){
+            toast.success("Allowed API Route!")
+        }
+        else{
+            toast.error("Forbidden API Route!")
+        }
+    }
+    
+    const onServerActionClick = () => {
+    admin()
+    .then((data)=>{
+        if(data?.error){
+            toast.error("Forbidden Server Action!")
+        }
+        else{
+            toast.success("Allowed Server Action!")
+        }
+    })
+    }
+
+    const role =  useCurrentRole();
+    return ( 
+        <Card className="w-[600px] mt-4 backdrop-blur-sm bg-black">
+        <CardHeader>
+            <p className="text-center items-center text-2xl font-semibold">
+                Admin 🔑
+            </p>
+        </CardHeader>
+        <CardContent>
+        <div className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+        <FormSuccess message="You are allowed to see this content"/>
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between
+        p-3 shadow-md rounded-lg border">
+            <p className="text-sm font-medium">ADMIN-only API Route</p>
+            <Button
+            variant="outline"
+            onClick={onApiRouteClick}
+            >
+                Test Here
+            </Button>
+        </div>
+        <div className="flex flex-row items-center justify-between
+        p-3 shadow-md rounded-lg border">
+            <p className="text-sm font-medium">ADMIN-only Server Action</p>
+            <Button
+            variant="outline"
+            onClick = {onServerActionClick}
+            >
+                Test Here
+            </Button>
+        </div>
+        </div>
+        </CardContent>
+        </Card>
+
+    );
+}
+
+export default AdminPage;
---
+++ app/api/admin/route.ts
@@ -0,0 +1,12 @@
+
+import { CurrentRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    const role = await CurrentRole();
+    if(role === UserRole.ADMIN){ 
+        return new NextResponse(null,{status:200})
+    }
+    return new NextResponse(null,{status:403})
+}
