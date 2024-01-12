5eee4c95bd90a2dd6b2a0f8404f746da60dfcdd7
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-12 18:25:24 +0530

    fix:oAuth check

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,7 +3,8 @@ import NextAuth,{DefaultSession} from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
     role:UserRole,
-    isTwoFactorEnabled:Boolean
+    isTwoFactorEnabled:boolean,
+    isOAuth:boolean
 }
 
 declare module "next-auth" {
