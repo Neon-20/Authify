04af11294fd7aa225fb6f214363dbd6e8ac5c114
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-12 18:25:08 +0530

    feat:adding oAuth check

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import authConfig from "./auth.config"
 import { db } from "./lib/db"
 import { getUserById } from './data/user';
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation"
+import { getAccountByUserId } from "./data/account"
 
 const prisma = new PrismaClient()
 
@@ -77,6 +78,12 @@ callbacks:{
         if(session.user){
             session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
         }
+
+        if(session.user){
+            session.user.name = token.name
+            session.user.email = token.email
+            session.user.isOAuth = token.isOAuth as boolean
+        }
         return session
     },
 
@@ -88,6 +95,12 @@ callbacks:{
         if(!existingUser){
             return token
         }
+
+        const existingAccount = await getAccountByUserId(existingUser.id)
+        
+        token.isOAuth = !!existingAccount
+        token.name = existingUser.name
+        token.email = existingUser.email
         token.role = existingUser.role
         token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
         return token
