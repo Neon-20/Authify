169bdda7cda992bf0a2709747df828c87e771fac
Pranav Rajveer <rajveer23pr@gmail.com>
2024-01-14 02:13:43 +0530

    fix: added next_public_app_url for resend

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,8 @@ const resend = new Resend(
     process.env.RESEND_API_KEY
 )
 
+const domain = process.env.NEXT_PUBLIC_APP_URL
+
 export const sendTwoFactorEmail = async(email:string,token:string) => {
     await resend.emails.send({
         from:"onboarding@resend.dev",
@@ -15,7 +17,7 @@ export const sendTwoFactorEmail = async(email:string,token:string) => {
 
 
 export const sendPasswordResetEmail = async(email:string,token:string) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+    const resetLink = `${domain}/auth/new-password?token=${token}`
     await resend.emails.send({
         from:"onboarding@resend.dev",
         to:email,
@@ -26,7 +28,7 @@ export const sendPasswordResetEmail = async(email:string,token:string) => {
 
 
 export const sendVerificationEmail = async(email:string,token:string) =>{
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
     console.log("Sent mail from here")
     await resend.emails.send({
         from:"onboarding@resend.dev",
